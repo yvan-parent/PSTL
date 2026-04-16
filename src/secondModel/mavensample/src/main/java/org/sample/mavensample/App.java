b90f107f245170186192811e38fb13c3e256d687
yvan-parent <yvan.parent77@gmail.com>
2026-04-16 23:36:02 +0200

    Update App.java

--- src/secondModel/mavensample/src/main/java/org/sample/mavensample/App.java
+++ src/secondModel/mavensample/src/main/java/org/sample/mavensample/App.java
@@ -28,12 +28,12 @@ public class App
 	 * @param player 		le modèle de l'instrumentiste
 	 * @param partition		le modèle de la pratition
 	 */
-	static int[][] findChords(ClasseGuitare guitare, ClasseGuitariste player, ClassePartition partition, boolean print, int LimitSeconds)
+	static int[][] findChords(ClasseGuitare guitare, ClasseGuitariste player, ClassePartition partition, boolean print, int maxMilli)
 	{
 		// 1. Définition du modèle
 		// 1.1. Définition
-		// Model model = new Model("Chord", Settings.init().setLCG(true));
-		Model model = new Model("Chord");
+		Model model = new Model("Chord", Settings.init().setLCG(true));
+		//Model model = new Model("Chord");
 		
 		long time1 = System.currentTimeMillis();
 		
@@ -60,39 +60,36 @@ public class App
 		// 1.3. on créé nos indéterminées en ajoutant les contraintes
 		IntVar[][] doigtes = ClasseOutilsMusique.ajoutContraintes(guitare, player, partition, model, positions_legales);
 
-		/* Affichage des positions légales
-		for (int i=0; i<partition.chords.length; i++)
-		{
-			System.out.println(positions_legales.get(partition.chords[i]));
-		}
-		*/
-
 		System.out.println("step 2");
 		
 		// 2. Résolution
 		Solver solver = model.getSolver();
-		//solver.setSearch(
+		/* si décommenté, processing prend beaucoup trop de temps, mais mis en commentaire de base */
+		// solver.setSearch(
         //        Search.lastConflict(Search.inputOrderLBSearch(ArrayUtils.flatten(doigtes)))
-        //);
+        // );
         /**/
 		System.out.println("step 3");
 
 		solver.setSearch(
                 Search.inputOrderLBSearch(ArrayUtils.flatten(doigtes))
         );
+		/* si décommenté, processing prend beaucoup trop de temps, mais mis en commentaire de base */
 		/**/
-		/*
-		solver.setSearch(
-        	Search.conflictOrderingSearch(
-                Search.inputOrderLBSearch(ArrayUtils.flatten(doigtes))
-        ));
-		*/
+		// solver.setSearch(
+        // 	Search.conflictOrderingSearch(
+        //         Search.inputOrderLBSearch(ArrayUtils.flatten(doigtes))
+        // ));
+		
 		System.out.println("step 4");
 		if (print) solver.showShortStatistics();
 		IntVar toMinimize = ClasseOutilsMusique.fonctionToMinimize(model, doigtes, guitare, partition, player, criteresStock);		// La quantité à minimiser
 		System.out.println("step 5");
-		solver.limitTime(LimitSeconds+"s");
+		solver.showStatistics();
+				long time = System.currentTimeMillis();
+		if (maxMilli>0) { solver.limitTime(maxMilli); }
 		Solution solution = solver.findOptimalSolution(toMinimize, Model.MINIMIZE);		// Une solution minimisant toMinimize
+				System.out.println("Resolution time: " + (System.currentTimeMillis() - time) + " ms");
 		System.out.println("step 6");
 
 		if (solution == null)
@@ -114,7 +111,7 @@ public class App
 		}
 		System.out.println("step 7");
 		if (print) { System.out.println("toMinimize ---> "+solution.getIntVal(toMinimize)); }
-
+		
 		return chords_v;
 	}
 
@@ -127,65 +124,24 @@ public class App
 	 */
     public static void main( String[] args )
     {
-		// 1. Model of the guitar
-		ClasseGuitare guitare = new ClasseGuitare();
-                // Pour une guitare classique
-        guitare.nf = 13;
-        guitare.nc = 6;
-        guitare.scordatura = new int[]{52, 57, 62, 67, 71, 76};
-
-		// 2. Model of the player
-		ClasseGuitariste player = new ClasseGuitariste();
-		player.distanceFirstFrette = 4;							// Max distance from the first fret
-		player.nbDoigts = 4;									// Number of finger excluding the thumb
-		player.barre = 3;										// Wether barrés are possible -1 impossible, else number of fret after the barre in first position
-		
-		player.preferences = new ClassePreferences();
-		player.preferences.fretteMoyenneFix = false;
-		player.preferences.fretteMoyenne = 0;
-		player.preferences.fretteMoyennePoids = 100;
-
-		player.preferences.fondaBassFix = false;
-		player.preferences.fondaBassPoids = 0;
-		
-		player.preferences.cordesNonJoueesFix = true;
-		player.preferences.cordesNonJoueesMax = 1;
-
-		player.preferences.cordesAVidesFix = false;
-		player.preferences.cordesAVidesPoids = 0;
-
-		player.preferences.distanceMaxSouhaitee = 3;
-		player.preferences.distanceMaxSouhaiteePoids = 5;
-
-		player.preferences.doigtsUtilisesFix = true;
-		player.preferences.doigtsUtilisesMaxNb = 3;
-
-		player.preferences.norme1Fix = true;
-		player.preferences.norme1Poids = 5;
-
-		player.preferences.pasRepetitionFix = false;
-		player.preferences.pasRepetitionMax = 7;
-
-
-		// 3. Modele de la suite d'accords
-		ClassePartition partition = new ClassePartition();
-		// Les Feuilles Mortes Y.M J.P J.K
-		partition.chords = new int[][]{
+		int[][] partition = new int[][]{
 			{57, 0, 3, 7, 10},
-			{62, 0, 4, 7, 10},										
+			{62, 0, 4, 7, 10},                    
 			{67, 0, 4, 7},
 			{57, 0, 3, 7, 10},
-			{59, 0, 4, 7, 10},
-			{64, 0, 3, 7},
+			{64, 0, 3, 7}
 		};
 
 		// 4. Resolution 
-		long time = System.currentTimeMillis();
-		int[][] res = findChords(guitare, player, partition, true, 1000);
-		System.out.println("Resolution time: " + (System.currentTimeMillis() - time) + " ms");
+		App.getInfos(partition, true);
 	}
 
-	public static int[][] getInfos (int[][] partition_chords, int TimeLimit) {
+	public static int[][] getInfos (int[][] partition_chords, boolean print) {
+		return App.getInfosWithTimeLimit(partition_chords, print, -1);
+	}
+
+	
+	public static int[][] getInfosWithTimeLimit (int[][] partition_chords, boolean print, int maxMilli) {
 		// 1. Model of the guitar
 		ClasseGuitare guitare = new ClasseGuitare();
 		// Pour une guitare classique
@@ -240,8 +196,8 @@ public class App
 		partition.chords = partition_chords;
 
 		// 4. Resolution
-		int[][] res = findChords(guitare, player, partition, false, TimeLimit);
+		int[][] res = findChords(guitare, player, partition, print, (maxMilli>0?maxMilli:-1));
 		return res;
 	}
-	
+
 }
